a4b2d476929227c046d999a2f73ccb6b5c9d9459
Nikola <jakshic.nikola@gmail.com>
2018-03-03 14:03:18 +0100

    bugfix search history list is broken after rotation

--- app/src/main/java/com/nikola/jakshic/truesight/view/activity/SearchActivity.java
+++ app/src/main/java/com/nikola/jakshic/truesight/view/activity/SearchActivity.java
@@ -122,6 +122,7 @@ public class SearchActivity extends AppCompatActivity implements SearchHistoryAd
         });
 
         mSearchView.setOnQueryTextFocusChangeListener((v, hasFocus) -> {
+            mFocus = hasFocus;
             // Show search history when search view has focus
             TransitionManager.beginDelayedTransition(rootView);
             recViewSearchHistory.setVisibility(hasFocus ? View.VISIBLE : View.INVISIBLE);
