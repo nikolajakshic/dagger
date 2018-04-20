471e7974c9bba9e21df6528762c76c67b3e8ae69
Nikola <jakshic.nikola@gmail.com>
2018-04-20 18:30:06 +0200

    home activity converted to kotlin

--- app/src/main/java/com/nikola/jakshic/dagger/extensions.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/extensions.kt
@@ -1,8 +1,24 @@
 package com.nikola.jakshic.dagger
 
 import android.app.Activity
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.widget.Toast
 
 fun Activity.toast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, msg, duration).show()
+}
+
+fun Fragment.show(transition: Int = FragmentTransaction.TRANSIT_FRAGMENT_FADE) {
+    fragmentManager?.beginTransaction()
+            ?.show(this)
+            ?.setTransition(transition)
+            ?.commit()
+}
+
+fun Fragment.hide(transition: Int = FragmentTransaction.TRANSIT_FRAGMENT_FADE) {
+    fragmentManager?.beginTransaction()
+            ?.hide(this)
+            ?.setTransition(transition)
+            ?.commit()
 }
---
+++ app/src/main/java/com/nikola/jakshic/dagger/view/activity/HomeActivity.kt
@@ -0,0 +1,91 @@
+package com.nikola.jakshic.dagger.view.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.nikola.jakshic.dagger.R
+import com.nikola.jakshic.dagger.hide
+import com.nikola.jakshic.dagger.show
+import com.nikola.jakshic.dagger.view.fragment.BookmarkFragment
+import com.nikola.jakshic.dagger.view.fragment.CompetitiveFragment
+import com.nikola.jakshic.dagger.view.fragment.LeaderboardFragment
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val competitive: Fragment
+        val leaderboard: Fragment
+        val bookmark: Fragment
+
+        if (savedInstanceState == null) {
+            competitive = CompetitiveFragment()
+            leaderboard = LeaderboardFragment()
+            bookmark = BookmarkFragment()
+
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.home_fragment_container, competitive, "competitive-tag")
+                    .add(R.id.home_fragment_container, leaderboard, "leaderboard-tag")
+                    .add(R.id.home_fragment_container, bookmark, "bookmark-tag")
+                    .hide(leaderboard)
+                    .hide(bookmark)
+                    .commit()
+        } else {
+            competitive = supportFragmentManager.findFragmentByTag("competitive-tag")
+            leaderboard = supportFragmentManager.findFragmentByTag("leaderboard-tag")
+            bookmark = supportFragmentManager.findFragmentByTag("bookmark-tag")
+        }
+
+        btmNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_competitive -> {
+                    competitive.show()
+                    leaderboard.hide()
+                    bookmark.hide()
+                    title = "Competitive"
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_leaderboard -> {
+                    competitive.hide()
+                    leaderboard.show()
+                    bookmark.hide()
+                    title = "Leaderboard"
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_bookmark -> {
+                    competitive.hide()
+                    leaderboard.hide()
+                    bookmark.show()
+                    title = "Bookmark"
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> return@setOnNavigationItemSelectedListener false
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.menu_home_search -> {
+                startActivity(Intent(this, SearchActivity::class.java))
+                true
+            }
+            R.id.menu_home_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
