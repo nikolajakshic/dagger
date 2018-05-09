83bd8d15a36f7d8d753eebe37d6d610694c641d1
Nikola <jakshic.nikola@gmail.com>
2018-05-09 18:16:53 +0200

    versioning

--- app/src/main/java/com/nikola/jakshic/dagger/data/local/DotaDatabase.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/data/local/DotaDatabase.kt
@@ -16,7 +16,7 @@ import com.nikola.jakshic.dagger.vo.Match
         Bookmark::class,
         MatchStats::class,
         PlayerStats::class
-), version = 3, exportSchema = false)
+), version = 4, exportSchema = false)
 abstract class DotaDatabase : RoomDatabase() {
 
     abstract fun competitiveDao(): CompetitiveDao
