0ad9dd8c1adbf93e984c0a1c5d833c64f5300070
Nikola <jakshic.nikola@gmail.com>
2018-05-18 10:29:13 +0200

    hero - decoupled view-model and repo

--- app/src/main/java/com/nikola/jakshic/dagger/repository/HeroRepository.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/repository/HeroRepository.kt
@@ -1,10 +1,11 @@
 package com.nikola.jakshic.dagger.repository
 
-import android.arch.lifecycle.MutableLiveData
-import com.nikola.jakshic.dagger.ui.Status
+import android.arch.lifecycle.LiveData
 import com.nikola.jakshic.dagger.data.local.HeroDao
 import com.nikola.jakshic.dagger.data.remote.OpenDotaService
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
@@ -15,30 +16,51 @@ class HeroRepository @Inject constructor(
         private val dao: HeroDao,
         private val service: OpenDotaService) {
 
-    fun fetchHeroes(status: MutableLiveData<Status>, id: Long): Disposable {
-        status.value = Status.LOADING
+    /**
+     * Constructs the [LiveData] which emits every time
+     * the requested data in the database has changed
+     */
+    fun getHeroesLiveDataByGames(id: Long) = dao.getHeroesByGames(id)
 
+    /**
+     * Constructs the [LiveData] which emits every time
+     * the requested data in the database has changed
+     */
+    fun getHeroesLiveDataByWinrate(id: Long) = dao.getHeroesByWinrate(id)
+
+    /**
+     * Constructs the [LiveData] which emits every time
+     * the requested data in the database has changed
+     */
+    fun getHeroesLiveDataByWins(id: Long) = dao.getHeroesByWins(id)
+
+    /**
+     * Constructs the [LiveData] which emits every time
+     * the requested data in the database has changed
+     */
+    fun getHeroesLiveDataByLosses(id: Long) = dao.getHeroesByLosses(id)
+
+    /**
+     * Fetches the matches from the network and inserts them into database.
+     *
+     * Whenever the database is updated, the observers of [LiveData]
+     * returned by [getHeroesLiveDataByGames], [getHeroesLiveDataByWins],
+     * [getHeroesLiveDataByLosses] and [getHeroesLiveDataByWinrate] are notified.
+     *
+     * @param onSuccess called on main thread
+     * @param onError called on main thread
+     */
+    fun fetchHeroes(id: Long, onSuccess: () -> Unit, onError: () -> Unit): Disposable {
         return service.getHeroes(id)
                 .subscribeOn(Schedulers.io())
                 .flatMap { Observable.fromIterable(it) }
                 .map {
-                    it.accountId = id
-                    it
+                    it.accountId = id   // response from the network doesn't contain any information
+                    it           // about who played this heroes, so we need to set this manually
                 }
                 .toList()
-                .subscribe({
-                    dao.insertHeroes(it)
-                    status.postValue(Status.SUCCESS)
-                }, {
-                    status.postValue(Status.ERROR)
-                })
+                .flatMapCompletable { Completable.fromAction { dao.insertHeroes(it) } }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ onSuccess() }, { onError() })
     }
-
-    fun sortByGames(id: Long) = dao.getHeroesByGames(id)
-
-    fun sortByWinRate(id: Long) = dao.getHeroesByWinrate(id)
-
-    fun sortByWins(id: Long) = dao.getHeroesByWins(id)
-
-    fun sortByLosses(id: Long) = dao.getHeroesByLosses(id)
 }
--- app/src/main/java/com/nikola/jakshic/dagger/ui/profile/heroes/HeroViewModel.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/ui/profile/heroes/HeroViewModel.kt
@@ -9,13 +9,16 @@ import com.nikola.jakshic.dagger.repository.HeroRepository
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
-class HeroViewModel @Inject constructor(private val repository: HeroRepository) : ViewModel() {
+class HeroViewModel @Inject constructor(
+        private val repository: HeroRepository) : ViewModel() {
 
     lateinit var list: LiveData<List<Hero>>
         private set
     val status = MutableLiveData<Status>()
     private val disposables = CompositeDisposable()
     private var initialFetch = false
+    private val onSuccess: () -> Unit = { status.value = Status.SUCCESS }
+    private val onError: () -> Unit = { status.value = Status.ERROR }
 
     fun initialFetch(id: Long){
         if (!initialFetch){
@@ -26,23 +29,24 @@ class HeroViewModel @Inject constructor(private val repository: HeroRepository) 
     }
 
     fun fetchHeroes(id: Long) {
-        disposables.add(repository.fetchHeroes(status, id))
+        status.value = Status.LOADING
+        disposables.add(repository.fetchHeroes(id, onSuccess, onError))
     }
 
     fun sortByGames(id: Long) {
-        list = repository.sortByGames(id)
+        list = repository.getHeroesLiveDataByGames(id)
     }
 
     fun sortByWinRate(id: Long) {
-        list = repository.sortByWinRate(id)
+        list = repository.getHeroesLiveDataByWinrate(id)
     }
 
     fun sortByWins(id: Long) {
-        list = repository.sortByWins(id)
+        list = repository.getHeroesLiveDataByWins(id)
     }
 
     fun sortByLosses(id: Long) {
-        list = repository.sortByLosses(id)
+        list = repository.getHeroesLiveDataByLosses(id)
     }
 
     override fun onCleared() {
