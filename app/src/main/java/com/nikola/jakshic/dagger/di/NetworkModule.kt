86157f8fbc3ba98b7b9a5d815f6d217c758e8e3e
Nikola <jakshic.nikola@gmail.com>
2018-06-16 02:47:11 +0200

    removed rxjava dependencies

--- app/src/main/java/com/nikola/jakshic/dagger/di/NetworkModule.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/di/NetworkModule.kt
@@ -11,7 +11,6 @@ import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
@@ -37,7 +36,6 @@ class NetworkModule(private val context: Context) {
         return Retrofit.Builder()
                 .baseUrl(OpenDotaService.BASE_URL)
                 .addCallAdapterFactory(CoroutineCallAdapterFactory())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(okHttpClient)
                 .build()
