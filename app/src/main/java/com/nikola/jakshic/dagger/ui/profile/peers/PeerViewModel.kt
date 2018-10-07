3daf676b593e962b2d7368bec2f56a6aab49d9b1
Nikola <jakshic.nikola@gmail.com>
2018-10-07 21:34:24 +0200

    fixed typo in variable name

--- app/src/main/java/com/nikola/jakshic/dagger/ui/profile/peers/PeerViewModel.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/ui/profile/peers/PeerViewModel.kt
@@ -17,14 +17,14 @@ class PeerViewModel @Inject constructor(private val repository: PeerRepository) 
     private val _status = MutableLiveData<Status>()
     val status: LiveData<Status>
         get() = _status
-    private var initialFatch = false
+    private var initialFetch = false
     private val jobs = Job()
     private val onSuccess: () -> Unit = { _status.value = Status.SUCCESS }
     private val onError: () -> Unit = { _status.value = Status.ERROR }
 
     fun initialFetch(id: Long) {
-        if (!initialFatch) {
-            initialFatch = true
+        if (!initialFetch) {
+            initialFetch = true
             fetchPeers(id)
             list = repository.getPeersLiveDataByGames(id)
         }
