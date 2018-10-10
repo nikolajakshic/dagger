e14a1efe4c078df72ee39312e38d75e5430f96cc
Nikola <jakshic.nikola@gmail.com>
2018-10-10 21:08:20 +0200

    enabled TLS 1.2 for older devices #10

--- app/src/main/java/com/nikola/jakshic/dagger/di/NetworkModule.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/di/NetworkModule.kt
@@ -1,22 +1,27 @@
 package com.nikola.jakshic.dagger.di
 
 import android.content.Context
+import android.os.Build
+import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
 import com.nikola.jakshic.dagger.data.remote.OpenDotaService
+import com.nikola.jakshic.dagger.data.remote.Tls12SocketFactory
 import dagger.Module
 import dagger.Provides
-import okhttp3.Cache
-import okhttp3.OkHttpClient
+import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
-import okhttp3.CipherSuite
-import okhttp3.ConnectionSpec
-
+import java.security.KeyStore
+import java.util.*
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
+import kotlin.Exception
 
 @Module
 class NetworkModule(private val context: Context) {
@@ -48,6 +53,11 @@ class NetworkModule(private val context: Context) {
     @Provides
     @Singleton
     fun provideOkHttpClient(): OkHttpClient {
+        val clientBuilder = OkHttpClient.Builder()
+                .cache(Cache(context.cacheDir, (10 * 1000 * 1000).toLong()))
+                .readTimeout(35, TimeUnit.SECONDS)
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
+
         val cipherSuites = ArrayList<CipherSuite>()
         cipherSuites.addAll(ConnectionSpec.MODERN_TLS.cipherSuites()!!)
         cipherSuites.add(CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA)
@@ -57,11 +67,30 @@ class NetworkModule(private val context: Context) {
                 .cipherSuites(*cipherSuites.toTypedArray())
                 .build()
 
-        val clientBuilder = OkHttpClient.Builder()
-                .cache(Cache(context.cacheDir, (10 * 1000 * 1000).toLong()))
-                .readTimeout(35, TimeUnit.SECONDS)
-                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
-                .connectionSpecs(listOf(legacyTls, ConnectionSpec.CLEARTEXT))
+        // Add obsolete Cipher Suites because the host we are trying to reach doesn't have the right ones
+        clientBuilder.connectionSpecs(listOf(legacyTls, ConnectionSpec.CLEARTEXT))
+
+        if (Build.VERSION.SDK_INT in 16..21) {
+            try {
+                val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+                trustManagerFactory.init(null as KeyStore?)
+
+                val trustManagers = trustManagerFactory.trustManagers
+                if (trustManagers.size != 1 || trustManagers[0] !is X509TrustManager) {
+                    throw IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers))
+                }
+
+                val trustManager = trustManagers[0] as X509TrustManager
+
+                val sslContext = SSLContext.getInstance("TLSv1.2")
+                sslContext.init(null, null, null)
+
+                // Enable TLS 1.2 for older devices
+                clientBuilder.sslSocketFactory(Tls12SocketFactory(sslContext.socketFactory), trustManager)
+            } catch (e: Exception) {
+                Log.e("NetworkModule", "Error while setting TLS 1.2: ", e)
+            }
+        }
 
         return clientBuilder.build()
     }
