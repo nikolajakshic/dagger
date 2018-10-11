b9a4afbe49940ef395535fc950ce47130646124c
Nikola <jakshic.nikola@gmail.com>
2018-10-11 22:26:41 +0200

    crashlytics - migrated to firebase

--- app/src/main/java/com/nikola/jakshic/dagger/ui/HomeActivity.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/ui/HomeActivity.kt
@@ -17,9 +17,9 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-         val competitive: Fragment
-         val leaderboard: Fragment
-         val bookmark: Fragment
+        val competitive: Fragment
+        val leaderboard: Fragment
+        val bookmark: Fragment
 
         if (savedInstanceState == null) {
             competitive = CompetitiveFragment()
