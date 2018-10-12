78dffa2b1063291d4b675b880eab3a861e32d96b
Nikola <jakshic.nikola@gmail.com>
2018-10-12 11:55:37 +0200

    match stats - added custom toolbar

--- app/src/main/java/com/nikola/jakshic/dagger/ui/matchStats/MatchStatsActivity.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/ui/matchStats/MatchStatsActivity.kt
@@ -2,13 +2,12 @@ package com.nikola.jakshic.dagger.ui.matchstats
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.nikola.jakshic.dagger.DaggerApp
 import com.nikola.jakshic.dagger.R
 import com.nikola.jakshic.dagger.ui.DaggerViewModelFactory
 import com.nikola.jakshic.dagger.ui.matchstats.overview.OverviewFragment
+import kotlinx.android.synthetic.main.activity_match_stats.*
 import javax.inject.Inject
 
 class MatchStatsActivity : AppCompatActivity() {
@@ -27,7 +26,7 @@ class MatchStatsActivity : AppCompatActivity() {
         }
 
         val id = intent.getLongExtra("match_id", -1)
-        title = "Match $id"
+        toolbar.title = "Match $id"
 
         val viewModel = ViewModelProviders.of(this, factory)[MatchStatsViewModel::class.java]
 
