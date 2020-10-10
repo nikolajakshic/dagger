3ef15fe50121330bca90ac97662281600624e6bb
Nikola <jakshic.nikola@gmail.com>
2020-10-10 14:59:33 +0200

    stream player - add picture-in-picture support

--- app/src/main/java/com/nikola/jakshic/dagger/stream/StreamPlayerActivity.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/stream/StreamPlayerActivity.kt
@@ -1,5 +1,7 @@
 package com.nikola.jakshic.dagger.stream
 
+import android.app.PictureInPictureParams
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -21,5 +23,29 @@ class StreamPlayerActivity : AppCompatActivity() {
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
         webView.loadUrl("https://player.twitch.tv/?channel=$userName&parent=twitch.tv&player=popout")
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val userName = intent?.getStringExtra("user-name")
+            ?: throw IllegalArgumentException("Intent with user-name extra must be passed.")
+        webView.loadUrl("https://player.twitch.tv/?channel=$userName&parent=twitch.tv&player=popout")
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            enterPictureInPictureMode(PictureInPictureParams.Builder().build())
+        }
+    }
+
+    override fun onBackPressed() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val isInPictureInPictureMode = enterPictureInPictureMode(PictureInPictureParams.Builder().build())
+            if (isInPictureInPictureMode) {
+                return
+            }
+        }
+        super.onBackPressed()
     }
 }
