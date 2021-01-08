1bdbd440e200637d5c7da01bcaeba8fa6eed00fb
Nikola <jakshic.nikola@gmail.com>
2021-01-08 12:18:08 +0100

    medal dialog - use coil to load images

--- app/src/main/java/com/nikola/jakshic/dagger/profile/MedalDialog.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/profile/MedalDialog.kt
@@ -1,19 +1,34 @@
 package com.nikola.jakshic.dagger.profile
 
-import android.app.Dialog
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import coil.load
 import com.nikola.jakshic.dagger.R
+import kotlinx.android.synthetic.main.dialog_medal.*
 
 class MedalDialog : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_medal, container, false)
+    }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-
-        return AlertDialog.Builder(requireContext())
-            .setTitle(getString(R.string.medals))
-            .setView(R.layout.dialog_medal)
-            .setPositiveButton(getString(R.string.close), null)
-            .create()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        imgHerald.load(R.drawable.ic_rank_1)
+        imgGuardian.load(R.drawable.ic_rank_2)
+        imgCrusader.load(R.drawable.ic_rank_3)
+        imgArchon.load(R.drawable.ic_rank_4)
+        imgLegend.load(R.drawable.ic_rank_5)
+        imgAncient.load(R.drawable.ic_rank_6)
+        imgDivine.load(R.drawable.ic_rank_7)
+        imgImmortal.load(R.drawable.ic_rank_8)
+        imgImmortalTop100.load(R.drawable.ic_rank_7a)
+        imgImmortalTop10.load(R.drawable.ic_rank_7b)
     }
 }
