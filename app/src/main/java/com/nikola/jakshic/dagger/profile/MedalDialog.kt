7331c6331fc893a381c98994234d0d495dae259a
Nikola <jakshic.nikola@gmail.com>
2021-07-04 14:28:43 +0200

    medal dialog - migrate from synthetics to 'view binding'

--- app/src/main/java/com/nikola/jakshic/dagger/profile/MedalDialog.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/profile/MedalDialog.kt
@@ -1,34 +1,26 @@
 package com.nikola.jakshic.dagger.profile
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import coil.load
 import com.nikola.jakshic.dagger.R
-import kotlinx.android.synthetic.main.dialog_medal.*
+import com.nikola.jakshic.dagger.databinding.DialogMedalBinding
 
-class MedalDialog : DialogFragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.dialog_medal, container, false)
-    }
-
+class MedalDialog : DialogFragment(R.layout.dialog_medal) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        imgHerald.load(R.drawable.ic_rank_1)
-        imgGuardian.load(R.drawable.ic_rank_2)
-        imgCrusader.load(R.drawable.ic_rank_3)
-        imgArchon.load(R.drawable.ic_rank_4)
-        imgLegend.load(R.drawable.ic_rank_5)
-        imgAncient.load(R.drawable.ic_rank_6)
-        imgDivine.load(R.drawable.ic_rank_7)
-        imgImmortal.load(R.drawable.ic_rank_8)
-        imgImmortalTop100.load(R.drawable.ic_rank_7a)
-        imgImmortalTop10.load(R.drawable.ic_rank_7b)
+        val binding = DialogMedalBinding.bind(view)
+
+        binding.imgHerald.load(R.drawable.ic_rank_1)
+        binding.imgGuardian.load(R.drawable.ic_rank_2)
+        binding.imgCrusader.load(R.drawable.ic_rank_3)
+        binding.imgArchon.load(R.drawable.ic_rank_4)
+        binding.imgLegend.load(R.drawable.ic_rank_5)
+        binding.imgAncient.load(R.drawable.ic_rank_6)
+        binding.imgDivine.load(R.drawable.ic_rank_7)
+        binding.imgImmortal.load(R.drawable.ic_rank_8)
+        binding.imgImmortalTop100.load(R.drawable.ic_rank_7a)
+        binding.imgImmortalTop10.load(R.drawable.ic_rank_7b)
     }
 }
