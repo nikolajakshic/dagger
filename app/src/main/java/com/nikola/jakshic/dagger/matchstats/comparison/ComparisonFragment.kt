60e122a45c99888b2f9ff77210e8130a729fcb13
Nikola <jakshic.nikola@gmail.com>
2021-07-04 12:54:54 +0200

    comparison fragment - migrate from synthetics to 'view binding'

--- app/src/main/java/com/nikola/jakshic/dagger/matchstats/comparison/ComparisonFragment.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/matchstats/comparison/ComparisonFragment.kt
@@ -8,18 +8,22 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import coil.load
 import com.nikola.jakshic.dagger.R
+import com.nikola.jakshic.dagger.databinding.FragmentComparisonBinding
 import com.nikola.jakshic.dagger.matchstats.MatchStatsUI
 import com.nikola.jakshic.dagger.matchstats.MatchStatsViewModel
 import com.nikola.jakshic.dagger.util.DotaUtil
 import com.nikola.jakshic.spiderchart.SpiderData
-import kotlinx.android.synthetic.main.fragment_comparison.*
 import java.util.concurrent.TimeUnit
 
 // Not using @AndroidEntryPoint, ViewModel is instantiated by parent-fragment.
-class ComparisonFragment : Fragment(R.layout.fragment_comparison), ComparisonDialog.ComparisonClickListener {
+class ComparisonFragment : Fragment(R.layout.fragment_comparison),
+    ComparisonDialog.ComparisonClickListener {
     private val viewModel by viewModels<MatchStatsViewModel>(
         ownerProducer = { requireParentFragment() }
     )
+
+    private var _binding: FragmentComparisonBinding? = null
+    private val binding get() = _binding!!
 
     private val MAX_HERO_DAMAGE_PER_MINUTE = 800F
     private val MAX_TOWER_DAMAGE_PER_MINUTE = 300F
@@ -46,6 +50,7 @@ class ComparisonFragment : Fragment(R.layout.fragment_comparison), ComparisonDia
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentComparisonBinding.bind(view)
 
         viewModel.match.observe(viewLifecycleOwner) { stats ->
             if (stats?.players?.size == 10) {
@@ -59,23 +64,29 @@ class ComparisonFragment : Fragment(R.layout.fragment_comparison), ComparisonDia
                         dialog = ComparisonDialog.newInstance(
                             leftPlayerIndex,
                             rightPlayerIndex,
-                            stats.players.map { it.heroId }.toList().toLongArray())
+                            stats.players.map { it.heroId }.toList().toLongArray()
+                        )
                         dialog?.setTargetFragment(this, 301)
                         dialog?.show(parentFragmentManager, null)
                     }
                 }
 
-                player1.setOnClickListener {
+                binding.player1.setOnClickListener {
                     SELECTED_PLAYER = SELECTED_PLAYER_LEFT
                     setupDialog()
                 }
 
-                player2.setOnClickListener {
+                binding.player2.setOnClickListener {
                     SELECTED_PLAYER = SELECTED_PLAYER_RIGHT
                     setupDialog()
                 }
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -99,7 +110,8 @@ class ComparisonFragment : Fragment(R.layout.fragment_comparison), ComparisonDia
             100 * player1.towerDamage.toFloat() / (MAX_TOWER_DAMAGE_PER_MINUTE * durationInMinutes),
             100 * player1.goldPerMin.toFloat() / (MAX_GOLD_PER_MINUTE),
             100 * player1.xpPerMin.toFloat() / (MAX_EXPERIENCE_PER_MINUTE),
-            100 * player1.heroDamage / (MAX_HERO_DAMAGE_PER_MINUTE * durationInMinutes))
+            100 * player1.heroDamage / (MAX_HERO_DAMAGE_PER_MINUTE * durationInMinutes)
+        )
 
         val entries2 = floatArrayOf(
             100 * player2.lastHits.toFloat() / (MAX_LAST_HITS_PER_MINUTE * durationInMinutes),
@@ -107,48 +119,56 @@ class ComparisonFragment : Fragment(R.layout.fragment_comparison), ComparisonDia
             100 * player2.towerDamage.toFloat() / (MAX_TOWER_DAMAGE_PER_MINUTE * durationInMinutes),
             100 * player2.goldPerMin.toFloat() / (MAX_GOLD_PER_MINUTE),
             100 * player2.xpPerMin.toFloat() / (MAX_EXPERIENCE_PER_MINUTE),
-            100 * player2.heroDamage / (MAX_HERO_DAMAGE_PER_MINUTE * durationInMinutes))
+            100 * player2.heroDamage / (MAX_HERO_DAMAGE_PER_MINUTE * durationInMinutes)
+        )
 
         val color1 = ContextCompat.getColor(requireContext(), R.color.comparison_player1)
         val color2 = ContextCompat.getColor(requireContext(), R.color.comparison_player2)
 
-        spiderChart.setData(listOf(SpiderData(entries1, color1), SpiderData(entries2, color2)))
-        spiderChart.setLabels(labels)
-        spiderChart.setRotationAngle(120f)
-        spiderChart.refresh()
+        binding.spiderChart.setData(
+            listOf(
+                SpiderData(entries1, color1),
+                SpiderData(entries2, color2)
+            )
+        )
+        binding.spiderChart.setLabels(labels)
+        binding.spiderChart.setRotationAngle(120f)
+        binding.spiderChart.refresh()
 
-        imgPlayer1Hero.load(DotaUtil.getHero(requireContext(), player1.heroId))
-        imgPlayer2Hero.load(DotaUtil.getHero(requireContext(), player2.heroId))
+        binding.imgPlayer1Hero.load(DotaUtil.getHero(requireContext(), player1.heroId))
+        binding.imgPlayer2Hero.load(DotaUtil.getHero(requireContext(), player2.heroId))
 
-        tvPlayer1Name.text = getPlayerName(player1)
-        tvPlayer2Name.text = getPlayerName(player2)
+        binding.tvPlayer1Name.text = getPlayerName(player1)
+        binding.tvPlayer2Name.text = getPlayerName(player2)
 
-        tvPlayer1Kda.text = getString(R.string.match_kda, player1.kills, player1.deaths, player1.assists)
-        tvPlayer2Kda.text = getString(R.string.match_kda, player2.kills, player2.deaths, player2.assists)
+        binding.tvPlayer1Kda.text =
+            getString(R.string.match_kda, player1.kills, player1.deaths, player1.assists)
+        binding.tvPlayer2Kda.text =
+            getString(R.string.match_kda, player2.kills, player2.deaths, player2.assists)
 
-        imgPlayer1Item0.load(DotaUtil.getItem(requireContext(), player1.item0))
-        imgPlayer1Item1.load(DotaUtil.getItem(requireContext(), player1.item1))
-        imgPlayer1Item2.load(DotaUtil.getItem(requireContext(), player1.item2))
-        imgPlayer1Item3.load(DotaUtil.getItem(requireContext(), player1.item3))
-        imgPlayer1Item4.load(DotaUtil.getItem(requireContext(), player1.item4))
-        imgPlayer1Item5.load(DotaUtil.getItem(requireContext(), player1.item5))
+        binding.imgPlayer1Item0.load(DotaUtil.getItem(requireContext(), player1.item0))
+        binding.imgPlayer1Item1.load(DotaUtil.getItem(requireContext(), player1.item1))
+        binding.imgPlayer1Item2.load(DotaUtil.getItem(requireContext(), player1.item2))
+        binding.imgPlayer1Item3.load(DotaUtil.getItem(requireContext(), player1.item3))
+        binding.imgPlayer1Item4.load(DotaUtil.getItem(requireContext(), player1.item4))
+        binding.imgPlayer1Item5.load(DotaUtil.getItem(requireContext(), player1.item5))
 
-        imgPlayer1Backpack0.load(DotaUtil.getItem(requireContext(), player1.backpack0))
-        imgPlayer1Backpack1.load(DotaUtil.getItem(requireContext(), player1.backpack1))
-        imgPlayer1Backpack2.load(DotaUtil.getItem(requireContext(), player1.backpack2))
-        imgPlayer1ItemNeutral.load(DotaUtil.getItem(requireContext(), player1.itemNeutral))
+        binding.imgPlayer1Backpack0.load(DotaUtil.getItem(requireContext(), player1.backpack0))
+        binding.imgPlayer1Backpack1.load(DotaUtil.getItem(requireContext(), player1.backpack1))
+        binding.imgPlayer1Backpack2.load(DotaUtil.getItem(requireContext(), player1.backpack2))
+        binding.imgPlayer1ItemNeutral.load(DotaUtil.getItem(requireContext(), player1.itemNeutral))
 
-        imgPlayer2Item0.load(DotaUtil.getItem(requireContext(), player2.item0))
-        imgPlayer2Item1.load(DotaUtil.getItem(requireContext(), player2.item1))
-        imgPlayer2Item2.load(DotaUtil.getItem(requireContext(), player2.item2))
-        imgPlayer2Item3.load(DotaUtil.getItem(requireContext(), player2.item3))
-        imgPlayer2Item4.load(DotaUtil.getItem(requireContext(), player2.item4))
-        imgPlayer2Item5.load(DotaUtil.getItem(requireContext(), player2.item5))
+        binding.imgPlayer2Item0.load(DotaUtil.getItem(requireContext(), player2.item0))
+        binding.imgPlayer2Item1.load(DotaUtil.getItem(requireContext(), player2.item1))
+        binding.imgPlayer2Item2.load(DotaUtil.getItem(requireContext(), player2.item2))
+        binding.imgPlayer2Item3.load(DotaUtil.getItem(requireContext(), player2.item3))
+        binding.imgPlayer2Item4.load(DotaUtil.getItem(requireContext(), player2.item4))
+        binding.imgPlayer2Item5.load(DotaUtil.getItem(requireContext(), player2.item5))
 
-        imgPlayer2Backpack0.load(DotaUtil.getItem(requireContext(), player2.backpack0))
-        imgPlayer2Backpack1.load(DotaUtil.getItem(requireContext(), player2.backpack1))
-        imgPlayer2Backpack2.load(DotaUtil.getItem(requireContext(), player2.backpack2))
-        imgPlayer2ItemNeutral.load(DotaUtil.getItem(requireContext(), player2.itemNeutral))
+        binding.imgPlayer2Backpack0.load(DotaUtil.getItem(requireContext(), player2.backpack0))
+        binding.imgPlayer2Backpack1.load(DotaUtil.getItem(requireContext(), player2.backpack1))
+        binding.imgPlayer2Backpack2.load(DotaUtil.getItem(requireContext(), player2.backpack2))
+        binding.imgPlayer2ItemNeutral.load(DotaUtil.getItem(requireContext(), player2.itemNeutral))
     }
 
     private fun getPlayerName(item: MatchStatsUI.PlayerStatsUI) = when {
