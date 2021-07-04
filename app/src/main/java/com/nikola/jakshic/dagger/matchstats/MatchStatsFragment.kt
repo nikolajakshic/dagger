7919d522b0fc4ce74b653091f06de3507ab2e154
Nikola <jakshic.nikola@gmail.com>
2021-07-04 12:57:42 +0200

    match stats fragment - migrate from synthetics to 'view binding'

--- app/src/main/java/com/nikola/jakshic/dagger/matchstats/MatchStatsFragment.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/matchstats/MatchStatsFragment.kt
@@ -10,49 +10,56 @@ import androidx.navigation.fragment.navArgs
 import com.nikola.jakshic.dagger.R
 import com.nikola.jakshic.dagger.common.Status
 import com.nikola.jakshic.dagger.common.toast
+import com.nikola.jakshic.dagger.databinding.ActivityMatchStatsBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_match_stats.*
 
 @AndroidEntryPoint
 class MatchStatsFragment : Fragment(R.layout.activity_match_stats) {
     private val viewModel by viewModels<MatchStatsViewModel>()
     private val args by navArgs<MatchStatsFragmentArgs>()
 
+    private var _binding: ActivityMatchStatsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = ActivityMatchStatsBinding.bind(view)
 
         // Change the color of the progress bar
-        progressBar.indeterminateDrawable.setColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY)
+        binding.progressBar.indeterminateDrawable.setColorFilter(
+            Color.WHITE,
+            PorterDuff.Mode.MULTIPLY
+        )
 
         val id = args.matchId
-        toolbar.title = "${getString(R.string.match)} $id"
+        binding.toolbar.title = "${getString(R.string.match)} $id"
 
         viewModel.initialFetch(id)
 
         viewModel.status.observe(viewLifecycleOwner) {
             when (it) {
                 Status.LOADING -> {
-                    btnRefresh.isEnabled = false
-                    btnRefresh.visibility = View.INVISIBLE
-                    progressBar.visibility = View.VISIBLE
+                    binding.btnRefresh.isEnabled = false
+                    binding.btnRefresh.visibility = View.INVISIBLE
+                    binding.progressBar.visibility = View.VISIBLE
                 }
                 else -> {
-                    progressBar.visibility = View.INVISIBLE
-                    btnRefresh.visibility = View.VISIBLE
-                    btnRefresh.isEnabled = true
+                    binding.progressBar.visibility = View.INVISIBLE
+                    binding.btnRefresh.visibility = View.VISIBLE
+                    binding.btnRefresh.isEnabled = true
                 }
             }
         }
 
         viewModel.isBookmarked.observe(viewLifecycleOwner) {
             if (it != 0L) {
-                imgBookmark.setImageResource(R.drawable.ic_match_note_bookmark_active)
+                binding.imgBookmark.setImageResource(R.drawable.ic_match_note_bookmark_active)
             } else {
-                imgBookmark.setImageResource(R.drawable.ic_match_note_bookmark_inactive)
+                binding.imgBookmark.setImageResource(R.drawable.ic_match_note_bookmark_inactive)
             }
         }
 
-        imgBookmark.setOnClickListener {
+        binding.imgBookmark.setOnClickListener {
             if (viewModel.isBookmarked.value != 0L) {
                 viewModel.removeFromBookmark(id)
             } else {
@@ -62,9 +69,14 @@ class MatchStatsFragment : Fragment(R.layout.activity_match_stats) {
             }
         }
 
-        btnRefresh.setOnClickListener { viewModel.fetchMatchStats(id) }
+        binding.btnRefresh.setOnClickListener { viewModel.fetchMatchStats(id) }
 
-        viewPager.adapter = MatchStatsPagerAdapter(requireContext(), childFragmentManager)
-        tabLayout.setupWithViewPager(viewPager)
+        binding.viewPager.adapter = MatchStatsPagerAdapter(requireContext(), childFragmentManager)
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
