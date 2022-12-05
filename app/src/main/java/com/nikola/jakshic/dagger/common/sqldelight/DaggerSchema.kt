e239f26e02383e458fe0058244935dc378185afe
Nikola Jaksic <jakshic.nikola@gmail.com>
2022-12-05 18:20:52 +0100

    dagger schema - close cursor

--- app/src/main/java/com/nikola/jakshic/dagger/common/sqldelight/DaggerSchema.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/common/sqldelight/DaggerSchema.kt
@@ -6,18 +6,21 @@ import com.squareup.sqldelight.db.SqlDriver
 object DaggerSchema : SqlDriver.Schema by Database.Schema {
     override fun migrate(driver: SqlDriver, oldVersion: Int, newVersion: Int) {
         if (oldVersion <= 8 && newVersion > 8) {
-            val cursor = driver.executeQuery(null, "SELECT name FROM sqlite_master WHERE type = 'table';", 0)
-            driver.execute(null, "PRAGMA foreign_keys = OFF;", 0)
-            while (cursor.next()) {
-                val table = cursor.getString(0)!!
-                // https://www.sqlite.org/fileformat.html#internal_schema_objects
-                if (!table.equals("android_metadata", ignoreCase = true) &&
-                    !table.startsWith("sqlite_", ignoreCase = true)
-                ) {
-                    driver.execute(null, "DROP TABLE IF EXISTS $table;", 0)
+            driver.executeQuery(
+                identifier = null,
+                sql = "SELECT name FROM sqlite_master WHERE type = 'table';",
+                parameters = 0
+            ).use { cursor ->
+                driver.execute(null, "PRAGMA foreign_keys = OFF;", 0)
+                while (cursor.next()) {
+                    val table = cursor.getString(0)!!
+                    // https://www.sqlite.org/fileformat.html#internal_schema_objects
+                    if (table != "android_metadata" && !table.startsWith("sqlite_")) {
+                        driver.execute(null, "DROP TABLE IF EXISTS $table;", 0)
+                    }
                 }
+                driver.execute(null, "PRAGMA foreign_keys = ON;", 0)
             }
-            driver.execute(null, "PRAGMA foreign_keys = ON;", 0)
             Database.Schema.create(driver)
         }
         Database.Schema.migrate(driver, oldVersion, newVersion)
