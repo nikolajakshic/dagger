7b2a79d6c985c896d9568e1255633dd7a25cbd97
Nikola Jaksic <jakshic.nikola@gmail.com>
2022-12-12 20:31:57 +0100

    profile view model - join declaration and assignment

--- app/src/main/java/com/nikola/jakshic/dagger/profile/ProfileViewModel.kt
+++ app/src/main/java/com/nikola/jakshic/dagger/profile/ProfileViewModel.kt
@@ -10,9 +10,9 @@ import com.squareup.sqldelight.runtime.coroutines.asFlow
 import com.squareup.sqldelight.runtime.coroutines.mapToOneOrNull
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flowOn
@@ -32,49 +32,44 @@ class ProfileViewModel @Inject constructor(
     private val dispatchers: Dispatchers,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
-    private val accountId: Long
+    private val accountId = ProfileFragmentArgs.fromSavedStateHandle(savedStateHandle).accountId
 
-    val player: Flow<PlayerUI?>
-    val isBookmarked: Flow<Boolean>
+    val player: StateFlow<PlayerUI?> = playerQueries.select(accountId)
+        .asFlow()
+        .mapToOneOrNull(dispatchers.io)
+        .filterNotNull()
+        .map { it.mapToUi() }
+        .retryWhen { _, attempt ->
+            delay(100)
+            return@retryWhen attempt < 3
+        }
+        .catch { Timber.e(it) }
+        .flowOn(dispatchers.io)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = null
+        )
+    val isBookmarked: StateFlow<Boolean> = playerBookmarkQueries.select(accountId)
+        .asFlow()
+        .mapToOneOrNull(dispatchers.io)
+        .map { it != null }
+        .retryWhen { _, attempt ->
+            delay(100)
+            return@retryWhen attempt < 3
+        }
+        .catch { Timber.e(it) }
+        .flowOn(dispatchers.io)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = false
+        )
 
     private val _isLoading = MutableStateFlow(false)
-    val isLoading: Flow<Boolean> = _isLoading
+    val isLoading: StateFlow<Boolean> = _isLoading
 
     init {
-        accountId = ProfileFragmentArgs.fromSavedStateHandle(savedStateHandle).accountId
-
-        player = playerQueries.select(accountId)
-            .asFlow()
-            .mapToOneOrNull(dispatchers.io)
-            .filterNotNull()
-            .map { it.mapToUi() }
-            .retryWhen { _, attempt ->
-                delay(100)
-                return@retryWhen attempt < 3
-            }
-            .catch { Timber.e(it) }
-            .flowOn(dispatchers.io)
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(5000),
-                initialValue = null
-            )
-        isBookmarked = playerBookmarkQueries.select(accountId)
-            .asFlow()
-            .mapToOneOrNull(dispatchers.io)
-            .map { it != null }
-            .retryWhen { _, attempt ->
-                delay(100)
-                return@retryWhen attempt < 3
-            }
-            .catch { Timber.e(it) }
-            .flowOn(dispatchers.io)
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(5000),
-                initialValue = false
-            )
-
         fetchProfile()
     }
 
